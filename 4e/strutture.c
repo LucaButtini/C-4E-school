32aac678a1b4ef45e85e2536c2c1799c66c8e803
Luca Buttini <luca.buttini@iisviolamarchesini.edu.it>
2023-10-05 10:48:37 +0200

    other changes

--- 4e/strutture.c
+++ 4e/strutture.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-// Definizione della struttura
+///definisco il tipo di struct e sotto creo variabile di quel tipo.
 struct s_punto
 {
     int x;
     int y;
     int z;
-} punto1; // Definizione di una variabile di quel tipo di struttura
-void stampa(struct s_punto p1)
+} p; 
+void stampaStruct(struct s_punto p1)//stampo struct
 {
     printf("\nX %d, Y %d, Z %d\n", p1.x, p1.y, p1.z);
 }
-void puntoMod(struct s_punto p1)
+void puntoModificato(struct s_punto p1)//modifico e stampo il punto modificato
 {
     p1.x++;
     p1.y++;
@@ -20,11 +20,11 @@ void puntoMod(struct s_punto p1)
 }
 int main(int argc, char *argv[])
 {
-    punto1.x = 5;
-    punto1.y = -2;
-    punto1.z = 7;
+    p.x = 5;
+    p.y = -2;
+    p.z = 7;
     printf("Punto 1");
-    stampa(punto1);
-    puntoMod(punto1);
+    stampaStruct(p);
+    puntoModificato(p);
     return 0;
 }
