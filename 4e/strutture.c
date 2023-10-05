40e1e723cd454e15aef82cc5a6b223869cff0396
Luca Buttini <luca.buttini@iisviolamarchesini.edu.it>
2023-10-05 10:37:49 +0200

    fixed strutture

--- 4e/strutture.c
+++ 4e/strutture.c
@@ -1,34 +1,30 @@
 #include <stdio.h>
 
-// definisco la struct
+// Definizione della struttura
 struct s_punto
 {
     int x;
     int y;
     int z;
-} punto1; // definisco una var di quel tipo struttura
-
-main(int argc, char *argv[])
-{
-    punto1.x = 5;
-    punto1.y = -2;
-    punto1.z = 7;
-    print("Punto 1")
-        stampa(punto1);
-
-    struct s_punto punto2 = puntoMod
-
-        return 0;
-}
-
+} punto1; // Definizione di una variabile di quel tipo di struttura
 void stampa(struct s_punto p1)
 {
-    printf("X %d, Y%d, Z %d", p1.x, p1.y, p1.z);
+    printf("\nX %d, Y %d, Z %d\n", p1.x, p1.y, p1.z);
 }
-
 void puntoMod(struct s_punto p1)
 {
     p1.x++;
     p1.y++;
     p1.z++;
+    printf("\nX %d, Y %d, Z %d\n", p1.x, p1.y, p1.z);
+}
+int main(int argc, char *argv[])
+{
+    punto1.x = 5;
+    punto1.y = -2;
+    punto1.z = 7;
+    printf("Punto 1");
+    stampa(punto1);
+    puntoMod(punto1);
+    return 0;
 }
