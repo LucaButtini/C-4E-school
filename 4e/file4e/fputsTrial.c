5f5c0e0da1d7056784633363698fe1f619f91b6c
Luca Buttini <luca.buttini@iisviolamarchesini.edu.it>
2023-10-26 10:32:58 +0200

    files

--- 4e/file4e/fputsTrial.c
+++ 4e/file4e/fputsTrial.c
@@ -1,34 +1,44 @@
+
 #include <stdio.h>
 #include <stdlib.h>
-// tastiera e monitor sono due file
-// tastiera di input e monitor output
-// stdi, stdout
 
-int main(int argc, char *argv[])
+#define MAX_STR 20  // Definizione di una costante MAX_STR con valore 20
+#define qtaNomi 4   // Definizione di una costante qtaNomi con valore 4
+int i;             // Dichiarazione di una variabile intera i
+
+int main()
 {
-    // è una libreria
-    FILE *nomi; // var di tipo puntatore a file
-    const int DIMARR = 100;
-    const int DIM = 4;
-    char vetNome[DIMARR];
-    nomi = fopen("nomi.txt", "a"); // apertura del file. Lo metto in w dato che lo devo scrivere. Fopen ritorna un valore
-    // se esiste già inizio con "a" metto dentro altri contenuti da dove eravamo arrivati
-    if (nomi == NULL) // se è cosi vuol dire che il file non si può aprire
+    FILE *fileDestinazione;  // Dichiarazione di un puntatore a un oggetto di tipo FILE
+    char vett[MAX_STR];      // Dichiarazione di un array di caratteri chiamato vett con dimensione MAX_STR
+
+    fileDestinazione = fopen("nomi.txt", "w");  // Apre il file in modalità scrittura 
+    if (fileDestinazione == NULL)  // Verifica se l'apertura del file ha avuto successo
     {
-        printf("File non aperto: errore\n");
-        exit(1); // esco dal programma
+        printf("Impossibile aprire il file");  // Stampa un messaggio di errore
+        exit(1);  // Termina il programma con codice di uscita 1, errore
     }
-    // mi si è creato nella directory corrente il file dato che in principio non c'era
-    //-------------------------------------------------------------------------------
-    // andiamo ora a scrivere nel file
-    for (int i = 0; i < DIM; i++)
+    else
     {
-        printf("Inserisci il nome %d\n", i + 1);
-        scanf("%s", vetNome);
-        fputs(vetNome, nomi); // origine del file che va grazie al puntatore nel file
-        fputc('\n', nomi);    // vado a capo
+        for (i = 0; i < qtaNomi; i++)  //  for da 0 a qtaNomi
+        {
+            printf("Inserisci il %d^ nome   >>   ", i + 1);  //messaggio
+            scanf("%s", vett);  // Legge una stringa 
+
+            fputs(vett, fileDestinazione);  // Scrive la stringa vett nel fileDestinazione
+            fputc('\n', fileDestinazione);  // Scrive un carattere di nuova riga nel file
+        }
     }
-    // il file va chiuso alla fine
-    fclose(nomi);
-    return 0;
+    fclose(fileDestinazione);  // Chiude il file
+
+    fileDestinazione = fopen("nomi.txt", "r");  // Apre il file  in modalità lettura 
+    while (fgets(vett, MAX_STR, fileDestinazione))  // Legge una riga alla volta dal file in vett
+        printf("%s", vett);  // Stampa la riga letta
+
+    if (fclose(fileDestinazione) == 0)  // Chiude il file e verifica se la chiusura ha avuto successo
+        printf("\n\nChiusura del file avvenuta con successo");  // Stampa un messaggio di chiusura riuscita
+    else
+        printf("\n\nChiusura del file fallita");  // Stampa un messaggio di chiusura fallita
+
+    return 0;  // Termina il programma restituendo 0 indicando una terminazione corretta
 }
+
