4cb201cad379a50c5a126fb1e05411ea820def5b
Luca Buttini <luca.buttini@iisviolamarchesini.edu.it>
2023-11-09 11:06:25 +0100

    p

--- 4e/pPadre_pFiglio.c
+++ 4e/pPadre_pFiglio.c
@@ -12,12 +12,15 @@ int p;
 
 int main()
 {
+    // la fork crea una copia del codice, hanno i PID diversi
     p = fork(); // restituisce un intero
     if (p != 0) // siamo nel padre
     {
+        printf("\nIo sono il padre, p=%d\n", p);
     }
     else // figlio
     {
+        printf("\nIo sono il figlio, p=%d\n", p);
     }
     return 0;
 }
