4c27cd6f4de39f6d67d1ec2c3427e06c637f229b
Luca Buttini <luca.buttini@iisviolamarchesini.edu.it>
2023-11-09 11:21:11 +0100

    p

--- 4e/pPadre_pFiglio.c
+++ 4e/pPadre_pFiglio.c
@@ -9,6 +9,7 @@
 // il codice del padre è condiviso con il figlio, tutto ciò che è presente nel padre sarà presente nel figlio. Sono indipendenti, zone di memoria differente
 // anche i figli possono generare altri figli
 // il p del padre è uguale a quello del figlio
+// tutti i processi in Linux hanno un padre, tranne quello Init lanciato dal kernel e serve per inizializzare il sistema operativo
 int p;
 
 int main()
@@ -17,11 +18,11 @@ int main()
     p = fork(); // restituisce un intero
     if (p != 0) // siamo nel padre
     {
-        printf("\nIo sono il padre, p=%d, PID=%d\n", p, getpid()); // funzione getpid permette di ottenere la visualizzazione del PID
-    }
+        printf("\nIo sono il padre, p=%d, PID=%d, mio padre ha PID uguale %d \n", p, getpid(), getppid()); // funzione getpid permette di ottenere la visualizzazione del PID. Il padre del padre è il terminale, la shell bash.
+        }
     else // figlio
     {
-        printf("\nIo sono il figlio, p=%d, PID=%d, mio padre ha PID uguale %d\n", p, getpid(), getppid()); // funzione getppidrestituisce pid del padre
+        printf("\nIo sono il figlio, p=%d, PID=%d, mio padre ha PID uguale %d\n", p, getpid(), getppid()); // funzione getppid restituisce pid del padre
     }
     return 0;
 }
