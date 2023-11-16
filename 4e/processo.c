799016fd36a52e30961f695dffc94056e0d64293
Luca Buttini <luca.buttini@iisviolamarchesini.edu.it>
2023-11-16 10:40:33 +0100

    processo

---
+++ 4e/processo.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+// restituisce un val intero.
+// p nel padre = PID figlio
+// p nel figlio=0
+// è dichiarata a livello locale e permette di gestire il padre e figlio
+// il codice del padre è condiviso con il figlio, tutto ciò che è presente nel padre sarà presente nel figlio. Sono indipendenti, zone di memoria differente
+// anche i figli possono generare altri figli
+// il p del padre è uguale a quello del figlio
+// tutti i processi in Linux hanno un padre, tranne quello Init lanciato dal kernel e serve per inizializzare il sistema operativo
+int p;
+
+// a livello globale, "p" potrebbe essere utilizzata anche in altre funzioni.
+// a livello locale dentro ad una funzione, al termine di essa viene deallocata
+int main()
+{
+    // int p;
+    //  la fork crea una copia del codice, hanno i PID diversi
+
+    printf("ciao\n");
+    // questa istruzione prima della funzione fork viene lo stesso eseguita, dopo vengono eseguite le istruzioni della fork
+
+    p = fork(); // restituisce un intero
+    if (p != 0) // siamo nel padre
+    {
+        printf("\nIo sono il processo padre, p=%d, PID=%d, mio padre ha PID uguale %d \n", p, getpid(), getppid()); // funzione getpid permette di ottenere la visualizzazione del PID. Il padre del padre è il terminale, la shell bash.
+    }
+    else // figlio
+    {
+        printf("\nIo sono il processo figlio, p=%d, PID=%d, mio padre ha PID uguale %d\n", p, getpid(), getppid()); // funzione getppid restituisce pid del padre
+    }
+    return 0;
+}
