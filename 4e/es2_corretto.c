3574f2d6c02ab97d4528dbb0a74aecba882bed5f
Luca Buttini <luca.buttini@iisviolamarchesini.edu.it>
2023-11-23 10:52:24 +0100

    .

---
+++ 4e/es2_corretto.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int p, i;
+
+int main(int argc, char *argv[])
+{
+    p = getpid();
+    printf("Sono il Padre: %d\n", p);
+
+    for (i = 0; i < 2; i++)
+    {
+        if (p != 0)
+        {
+            p = fork();
+            if (p == 0)
+            {
+                printf("\nsono il figlio %d\n", p);
+            }
+        }
+    }
+    return 0;
+}
