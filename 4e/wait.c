a4534c04dfefc1aa2cc6d570e8e99bf1f78bf8f1
Luca Buttini <luca.buttini@iisviolamarchesini.edu.it>
2023-11-30 11:07:35 +0100

    wait

--- 4e/wait.c
+++ 4e/wait.c
@@ -11,15 +11,17 @@ int main(int argc, char *argv[])
     p = fork();
     if (p == 0) // figlio
     {
-        printf("1_ Sono il figlio, pid: %d\n", getpid());
-        printf("2_ Sono il figlio e mio padre ha pid: %d\n", getppid());
+        printf("1_ Sono il figlio, pid: [%d]\n", getpid());
+        printf("2_ Sono il figlio e mio padre ha pid: [%d]\n", getppid());
         exit(25); // prima comunicazione, limitata,  fra figlio e padre
     }
     else // padre
     {
-        printf("4_ Sono il padre, pid: %d\n", getpid());
-        printf("5_ Sono il padre e mio figlio ha pid: %d\n\n", p);
-        printf("Attendo che mio figlio: %d termini\n\n", wait(&status));
+        printf("4_ Sono il padre, pid: [%d]\n", getpid());
+        printf("5_ Sono il padre e mio figlio ha pid: [%d]\n\n", p);
+        printf("Attendo che mio figlio: [%d] termini\n\n", wait(&status));
+        printf("Il codice di terminazione di mio figlio: WEXISTATUS(status) => [%d]\n", WEXITSTATUS(status));
+        printf("Attendo che mio figlio: [%d] termini\n\n", wait(&status));
     }
     return 0;
 }
