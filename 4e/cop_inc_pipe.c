f3ec272fea7f420ebae5b51aeb7baea1001959ea
Luca Buttini <luca.buttini@iisviolamarchesini.edu.it>
2024-02-08 10:54:44 +0100

    v2 con conta car

--- 4e/cop_inc_pipe.c
+++ 4e/cop_inc_pipe.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <sys/wait.h>
 #define BUFFER_DIM 1024
 
 int main(int argc, char *argv[])
@@ -20,58 +20,54 @@ int main(int argc, char *argv[])
     p = fork();
 
     if (p < 0)
-    {
-        printf("errore generazione figlio\n");
-        exit(-1);
-    }
 
-    if (p > 0) // padre
-    {
-        close(fd[0]); // chiudo lettura nel padre
-
-        if (argc != 3)
+        if (p > 0) // padre
         {
-            fprintf(stderr, "Inserire correttamente il numero di parametri\n");
-            exit(-1);
+            close(fd[0]); // chiudo lettura nel padre
+
+            if (argc != 3)
+            {
+                fprintf(stderr, "Inserire correttamente il numero di parametri\n");
+                exit(-1);
+            }
+
+            origine = fopen(argv[1], "r");
+
+            if (origine == NULL)
+            {
+                printf("Errore apertura file origine\n");
+                exit(-1);
+            }
+
+            while ((n = fread(buffer, 1, sizeof(buffer), origine)) > 0)
+            {
+                write(fd[1], buffer, n); // scrivo sulla pipe
+            }
+
+            fclose(origine);
+            close(fd[1]); // chiudo scrittura. Uscita pipe
         }
+        else
+        {                 // figlio
+            close(fd[1]); // chiudo scrittura nel figlio
 
-        origine = fopen(argv[1], "r");
+            destinazione = fopen(argv[2], "w");
 
-        if (origine == NULL)
-        {
-            printf("Errore apertura file origine\n");
-            exit(-1);
+            if (destinazione == NULL)
+            {
+                printf("Errore apertura file destinazione\n");
+                close(fd[0]);
+                exit(-1);
+            }
+
+            while ((n = read(fd[0], buffer, sizeof(buffer))) > 0)
+            {
+                fwrite(buffer, 1, n, destinazione); // leggo dalla pipe e scrivo nel file di destinazione
+            }
+
+            fclose(destinazione);
+            close(fd[0]); // chiudo lettura. Uscita pipe
         }
-
-        while ((n = fread(buffer, 1, sizeof(buffer), origine)) > 0)
-        {
-            write(fd[1], buffer, n); // scrivo sulla pipe
-        }
-
-        fclose(origine);
-        close(fd[1]); // chiudo scrittura. Uscita pipe
-    }
-    else
-    {                 // figlio
-        close(fd[1]); // chiudo scrittura nel figlio
-
-        destinazione = fopen(argv[2], "w");
-
-        if (destinazione == NULL)
-        {
-            printf("Errore apertura file destinazione\n");
-            close(fd[0]);
-            exit(-1);
-        }
-
-        while ((n = read(fd[0], buffer, sizeof(buffer))) > 0)
-        {
-            fwrite(buffer, 1, n, destinazione); // leggo dalla pipe e scrivo nel file di destinazione
-        }
-
-        fclose(destinazione);
-        close(fd[0]); // chiudo lettura. Uscita pipe
-    }
 
     return 0;
 }
