7ba6febbbb45b8feb9f7b8a196a07c281ab2b01c
vm-luca <luca.buttini@iisviolamarchesini.edu.it>
2024-03-06 12:22:21 +0100

    chat da finire

---
+++ 4e/progetto_chat/chat2.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#define DIM 1024
+
+int fd, p, n;
+unsigned char buffer[DIM];
+int main(int argc, char *argv[])
+{
+    do
+    {
+
+        p = fork();
+        if (p < 0)
+        {
+            printf("errore generazione figlio\n");
+            exit(-1);
+        }
+        if (p > 0) // padre
+        {
+            fd = open("fchat2", O_WRONLY);
+            scanf("%s", buffer);
+            n = write(fd, buffer, sizeof(buffer));
+            if (n < 0)
+            {
+                printf("errore scrittura\n");
+                exit(-1);
+            }
+            close(fd);
+        }
+        else // figlio
+        {
+            fd = open("fchat1", O_RDONLY);
+            n = read(fd, buffer, sizeof(buffer));
+            if (n < 0)
+            {
+                printf("errore lettura\n");
+                exit(-1);
+            }
+            printf("%s", buffer);
+            close(fd);
+        }
+    } while (1);
+    return 0;
+}
