56919f552f684926cb820cc386141b3389c367f4
vm-luca <luca.buttini@iisviolamarchesini.edu.it>
2024-04-13 16:54:47 +0200

    ricerca thread

---
+++ 4e/Threads/ricerca.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#define DIM 10
+
+void *ricerca_seconda(void *s)
+{
+    int *array = (int *)s;
+    // Esegue la ricerca nella seconda metà dell'array
+    for (int i = DIM / 2; i < DIM; i++)
+    {
+        if (array[i] == 8)
+        {
+            printf("Il numero 8 si trova nella seconda metà dell'array, in posizione: %d\n", i);
+        }
+    }
+    return NULL;
+}
+
+void *ricerca_prima(void *s)
+{
+    int *array = (int *)s;
+    // Esegue la ricerca nella prima metà dell'array
+    for (int i = 0; i < DIM / 2; i++)
+    {
+        if (array[i] == 8)
+        {
+            printf("Il numero 8 si trova nella prima metà dell'array, in posizione: %d\n", i);
+        }
+    }
+    return NULL;
+}
+
+int main()
+{
+    int array[DIM] = {3, 5, 2, 5, 6, 4, 1, 7, 8, 0}; // Array predefinito
+    pthread_t tPrima, tSeconda;
+
+    pthread_create(&tPrima, NULL, &ricerca_prima, (void *)array);
+    pthread_create(&tSeconda, NULL, &ricerca_seconda, (void *)array);
+
+    pthread_join(tPrima, NULL);
+    pthread_join(tSeconda, NULL);
+
+    return 0;
+}
