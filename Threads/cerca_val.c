d1bca6ff97491a0b8e4666f29d94c0c941cb26da
vm-luca <luca.buttini@iisviolamarchesini.edu.it>
2024-05-08 15:39:33 +0200

    ricerca valore

--- Threads/cerca_val.c
+++ Threads/cerca_val.c
@@ -5,90 +5,64 @@
 #define DIM_ARRAY 1000
 
 int array[DIM_ARRAY];
-int num;
+int numero;
 void *first(void *s)
 {
-    int presente = 0;
     for (int i = 0; i < DIM_ARRAY / 4; i++)
     {
-        if (array[i] == num)
+        if (array[i] == numero)
         {
-            printf("Il numero %d si trova in posizione: %d\n", num, i);
-            presente++;
+           printf("Il numero [%d] in posizione --> %d\n", numero, i);
         }
     }
-    if (presente == 0)
-    {
-        printf("Numero non presente nella prima metà\n");
-    }
-    return NULL;
+    pthread_exit(NULL);
 }
 
 void *second(void *s)
 {
-    int presente = 0;
     for (int i = DIM_ARRAY / 4; i < DIM_ARRAY / 2; i++)
     {
-        if (array[i] == num)
+        if (array[i] == numero)
         {
-            presente++;
-            printf("Il numero %d si trova in posizione: %d\n", num, i);
+            printf("Il numero [%d] in posizione --> %d\n", numero, i);
         }
     }
-    if (presente == 0)
-    {
-        printf("Numero non presente nella seconda metà\n");
-    }
-    return NULL;
+    pthread_exit(NULL);
 }
-
 void *third(void *s)
 {
-    int presente = 0;
     for (int i = DIM_ARRAY / 2; i < (DIM_ARRAY / 2 + DIM_ARRAY / 4); i++)
     {
-        if (array[i] == num)
+        if (array[i] == numero)
         {
-            printf("Il numero %d si trova in posizione: %d\n", num, i);
-            presente++;
+            printf("Il numero [%d] in posizione --> %d\n", numero, i);
         }
         
     }
-    if (presente == 0)
-    {
-        printf("Numero non presente nella terza metà\n");
-    }
-    return NULL;
+   pthread_exit(NULL);
 }
 
 void *fourth(void *s)
 {
-    int presente = 0;
     for (int i = (DIM_ARRAY / 2 + DIM_ARRAY / 4); i < DIM_ARRAY; i++)
     {
-        if (array[i] == num)
+        if (array[i] == numero)
         {
-            printf("Il numero %d si trova in posizione: %d\n", num, i);
-            presente ++;
+            printf("Il numero [%d] in posizione --> %d\n", numero, i);
         }
     }
-    if (presente == 0)
-    {
-        printf("Numero non presente nella quarta metà\n");
-    }
-    return NULL;
+    pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[])
 {
     pthread_t thread_prima, thread_seconda, thread_terza, thread_quarta;
-    printf("Inserire numero da ricercare: \n");
-    scanf("%d", &num);
     for (int i = 0; i < DIM_ARRAY; i++)
     {
-        int r = rand() % 500;
-        array[i] = r;
+        array[i] = rand() % 500;
     }
+    printf("Inserire numero da cercare: \n");
+    scanf("%d", &numero);
     pthread_create(&thread_prima, NULL, &first, NULL);
     pthread_create(&thread_seconda, NULL, &second, NULL);
     pthread_create(&thread_terza, NULL, &third, NULL);
